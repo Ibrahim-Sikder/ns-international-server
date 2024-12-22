fd1c34d3d02b3d1c35aea7085dc16e1639068c9c
Ibrahim Sikder <ibrahimsikder5033@gmail.com>
2024-12-22 15:41:16 +0600

    cors

--- src/app.ts
+++ src/app.ts
@@ -13,22 +13,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // const allowedOrigins = ['http://localhost:3001','http://localhost:3000'];
-const allowedOrigins = ['https://nsintbd.com/', 'https://admin.nsintbd.com/'];
+const allowedOrigins = ['https://nsintbd.com', 'https://admin.nsintbd.com'];
 
 app.use(
   cors({
     origin: (origin, callback) => {
+
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
         callback(new Error('Not allowed by CORS'));
       }
     },
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true, 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], 
     allowedHeaders: ['Content-Type', 'Authorization'],
   }),
 );
+
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to the API');
